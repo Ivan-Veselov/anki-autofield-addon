c27daa8d5113ae25b581dcb85398d08076d5952c
Ivan Veselov <veselov.iva@gmail.com>
2017-08-19 17:36:40 +0500

    Add shortcut for wrapping text with double brackets.

--- autofield.py
+++ autofield.py
@@ -1,13 +1,20 @@
+import sys # for debug purpose
 import re
+
+from aqt.qt import *
 
 from anki.hooks import addHook
 from aqt import mw
+from aqt.editor import Editor
+from anki.hooks import wrap
 
 gSourceFieldName = "Example [source]"
 gOpenedFieldName = "Example [opened]"
 gClozeFieldName = "Example [cloze]"
 
 gSyntaxPattetn = r'\[\[(.*?)\]\]'
+
+gShortcutKey = "F12"
 
 def onEditorFocusLost(aFlag, aNote, aFieldIndex):
     fieldNames = mw.col.models.fieldNames(aNote.model())
@@ -17,8 +24,8 @@ def onEditorFocusLost(aFlag, aNote, aFieldIndex):
 
     if gOpenedFieldName in fieldNames:
         aNote[gOpenedFieldName] = re.sub(gSyntaxPattetn,
-                                        r'<font color="#0000ff"><b>\1</b></font>',
-                                        aNote[gSourceFieldName])
+                                         r'<font color="#0000ff"><b>\1</b></font>',
+                                         aNote[gSourceFieldName])
         aFlag = True
 
     if gClozeFieldName in fieldNames:
@@ -29,4 +36,8 @@ def onEditorFocusLost(aFlag, aNote, aFieldIndex):
     
     return aFlag
 
+def editorInit(self, *args, **kwargs):
+    QShortcut(QKeySequence(gShortcutKey), self.widget, activated = lambda: self.web.eval("wrap('[[', ']]');"))
+
 addHook('editFocusLost', onEditorFocusLost)
+Editor.__init__ = wrap(Editor.__init__, editorInit)
