33a8f9bd3e0c5eec20832ec529f4f13aab6dfe10
Ivan Veselov <veselov.iva@gmail.com>
2017-08-19 17:59:41 +0500

    Fix issue with blinking fields

--- autofield.py
+++ autofield.py
@@ -16,24 +16,27 @@ gSyntaxPattetn = r'\[\[(.*?)\]\]'
 
 gShortcutKey = "F12"
 
+def updateDstField(aNote, aFieldName, aReplacement):
+    if not aFieldName in mw.col.models.fieldNames(aNote.model()):
+        return False
+    
+    newValue = re.sub(gSyntaxPattetn,
+                      r'<font color="#0000ff"><b>' + aReplacement + r'</b></font>', #tune
+                      aNote[gSourceFieldName])
+
+    result = aNote[aFieldName] != newValue
+    aNote[aFieldName] = newValue
+    return result
+
 def onEditorFocusLost(aFlag, aNote, aFieldIndex):
     fieldNames = mw.col.models.fieldNames(aNote.model())
     
     if fieldNames[aFieldIndex] != gSourceFieldName:
         return aFlag
 
-    if gOpenedFieldName in fieldNames:
-        aNote[gOpenedFieldName] = re.sub(gSyntaxPattetn,
-                                         r'<font color="#0000ff"><b>\1</b></font>',
-                                         aNote[gSourceFieldName])
-        aFlag = True
+    aFlag |= updateDstField(aNote, gOpenedFieldName, r'\1')
+    aFlag |= updateDstField(aNote, gClozeFieldName, r'[...]')
 
-    if gClozeFieldName in fieldNames:
-        aNote[gClozeFieldName] = re.sub(gSyntaxPattetn,
-                                        r'<font color="#0000ff"><b>[...]</b></font>',
-                                        aNote[gSourceFieldName])
-        aFlag = True
-    
     return aFlag
 
 def editorInit(self, *args, **kwargs):
